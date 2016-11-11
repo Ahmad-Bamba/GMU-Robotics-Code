f1039da0d075f674edb76bd0b93fede7646651f7
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2016-11-11 05:49:12 +0000

    Servo unit test

--- servo.py
+++ servo.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
+import time
 
-pin = 0
-cycle = 95
+pin = 17
+cycle = 0
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
@@ -10,7 +11,25 @@ pwm.start(cycle)
 
 try:
     while 1:
-        GPIO.output(pin, GPIO.LOW)
+        GPIO.output(pin, 0)
+	print "0"
+	time.sleep(1)
+	GPIO.output(pin, 5)
+	print "5"
+	time.sleep(1)
+	GPIO.output(pin, 10)
+	print "10"
+	time.sleep(1)
+	GPIO.output(pin, 50)
+	print "50"
+	time.sleep(1)
+	GPIO.output(pin, 75)
+	print "75"
+	time.sleep(1)
+	GPIO.output(pin, 128)
+	print "128"
+	time.sleep(1)
 except KeyboardInterrupt:
+    print "shrecking"
     pwm.stop()
     GPIO.cleanup()
