e8b22c62dcd2b47f0f2b5b94ba523b01a5e2c56e
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2016-11-24 05:55:33 +0000

    Working

--- servo.py
+++ servo.py
@@ -2,18 +2,28 @@ import RPi.GPIO as GPIO
 import time
 
 # Too bad python doesn't have const...
-
-pin = 17
-c = 95
+pin = 11
+c = 5
 
 # Set up GPIO stuff
 
-GPIO.setmod(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.OUT)
-pwm = GPIO.PWM(pin, 20)
+pwm = GPIO.PWM(pin, 50)
+
+# Left: 5, Center: 7.5, Right: 10
 
 pwm.start(c)
 
 try:
 	while 1:
-		
+		pwm.ChangeDutyCycle(7.5)
+		time.sleep(1)
+		pwm.ChangeDutyCycle(10)
+		time.sleep(1)
+		pwm.ChangeDutyCycle(5)
+		time.sleep(1)
+except KeyboardInterrupt:
+	pwm.stop()
+	GPIO.cleanup()
+	print "whatever"
