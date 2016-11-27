8fb6726d74f6931049ec2c24db8976a6392ea0c3
Ahmad Bamba <ahmad.eb.bamba@gmail.com>
2016-11-27 14:57:10 -0500

    Testing to see which port is what motor

--- robot.py
+++ robot.py
@@ -29,6 +29,10 @@ def init():
 
 def periodic():
     movinga = False
+    movingb = False
+    movingx = False
+    movingy = False
+
     enabled = True
     try:
         while enabled:
@@ -38,19 +42,47 @@ def periodic():
                     # Do button stuff here
                     if int(event.button) == 0: # A button
                         print "A"
-                        if not moving:
-                            print "cim.set(.2)"
-			    cim.set(.2)
-                            moving = True
+                        if not movinga:
+                            print "cim_top.set(.1)"
+			                cim_top.set(.1)
+                            movinga = True
                         else:
-                            print "cim.set(0)"
-			    cim.set(0)
-                            moving = False
-		    if int(event.button) == 2:
-			if
+                            print "cim_top.set(0)"
+			                cim_top.set(0)
+                            movinga = False
+		            elif int(event.button) == 2:
+			            print "B"
+                        if not movingb:
+                            print "cim_lef.set(.1)"
+                            cim_lef.set(.1)
+                            movingb = True
+                        else:
+                            print "cim_lef.set(0)"
+                            cim_lef.set(0)
+                            movingb = False
+                    elif int(event.button) == 3:
+                        print "X"
+                        if not movingx:
+                            print "cim_rig.set(.1)"
+                            cim_rig.set(.1)
+                            movingx = True
+                        else:
+                            print "cim_rig.set(0)"
+                            cim_rig.set(0)
+                            movingx = False
+                    elif int(event.button) == 4:
+                        print "Y"
+                        if not movingy:
+                            print "cim_bot.set(.1)"
+                            cim_bot.set(.1)
+                            movingy = True
+                        else:
+                            print "cim_bot.set(0)"
+                            cim_bot.set(0)
+                            movingy = False
     except KeyboardInterrupt:
         enabled = False
-	c.PWM.set_all_pwm(0, 0)
+	    c.PWM.set_all_pwm(0, 0)
         sys.exit()
 
 init()
