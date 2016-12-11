595227b7214fc10071d4975ae1c1972e5f46b9bc
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2016-12-11 13:13:43 -0500

    Server socket frameworkish

--- robot.py
+++ robot.py
@@ -7,17 +7,46 @@ import motor
 import pygame
 import sys
 import time
+import socket
+
+# Testing only
+import random
 
 # set period to 20ms, which is what the VictorSPs need
 c.PWM.set_pwm_freq(50)
 enabled = True
 stick = None
 
+# Networking
+robosock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+robosock.bind(socket.gethostname(), 9685)
+robosock.listen(5)
+
 # init hardware
 cim_top = motor.Victor(c.MOTOR_TOP)
 cim_bot = motor.Victor(c.MOTOR_BOTTOM)
 cim_lef = motor.Victor(c.MOTOR_LEFT)
 cim_rig = motor.Victor(c.MOTOR_RIGHT)
+
+
+def sendToDashboard(sock, msg):
+    totalsent = 0
+    while totalsent < len(msg):
+        sent = sock.send(msg[totalsent:])
+        if sent == 0:
+            raise RuntimeError("Socket connection broken!")
+        totalsent = totalsent + sent
+
+def recieveFromDashboard(sock, msg):
+    chunks = []
+    bytes_recd = 0
+    while bytes_recd < len(msg):
+        chunk = sock.recv(min(len(msg) - bytes_recd, 2048))
+        if chunk == '':
+            raise RuntimeError("Socket connection broken!")
+        chunks.append(chunk)
+        bytes_recd = bytes_recd + len(chunk)
+    return ''.join(chunks)
 
 def init():
 	global stick
@@ -32,45 +61,48 @@ def init():
 
 def periodic():
 	global stick
+	global enabled
+	global robosock
 
-	enabled = True
 	try:
-		while enabled:
-			pygame.event.pump()
-			for event in pygame.event.get():
-				if pygame.event.event_name(event.type) == "JoyButtonUp":
-					# Do button stuff here
-					if int(event.button) == 0: # A button
-						print "A"
-					elif int(event.button) == 1: # B Button
-						print "B"
-					elif int(event.button) == 2: # X Button?
-						print "X"
-					elif int(event.button) == 3: # Y Button?
-						print "Y"
+		while True:
+			(cli, address) = robosock.accept()
+			print "Recieved connection from " + str(address)
+			dashupdate = [ ["RobotState", str(enabled)],  ["Axis0", str(random.uniform(-1, 1))], ["Axis4", str(random.uniform(-1, 1))], ["Axis2", str(random.uniform(-1, 1))], ["Axis3", str(random.uniform(-1, 1))] ]
+			print str(dashupdate)
+			# sendToDashboard(str(dashupdate))
 
-			for i in xrange(stick.get_numaxes()):
-				if i == 1:
-					if abs(stick.get_axis(1)) > c.THRESHOLD:
-						cim_lef.set(stick.get_axis(1) * -1)
-						cim_rig.set(stick.get_axis(1))
-					else:
-						cim_lef.set(0)
-						cim_rig.set(0)
-				if i == 3:
-					if abs(stick.get_axis(3)) > c.THRESHOLD:
-						cim_top.set(stick.get_axis(3) * -1)
-						cim_bot.set(stick.get_axis(3))
-					else:
-						cim_top.set(0)
-						cim_bot.set(0)
+			if enabled:
+				pygame.event.pump()
+				for event in pygame.event.get():
+					if pygame.event.event_name(event.type) == "JoyButtonUp":
+						# Do button stuff here
+						if int(event.button) == 0: # A button
+							print "A"
+						elif int(event.button) == 1: # B Button
+							print "B"
+						elif int(event.button) == 2: # X Button?
+							print "X"
+						elif int(event.button) == 3: # Y Button?
+							print "Y"
+
+				for i in xrange(stick.get_numaxes()):
+					if i == 1:
+						if abs(stick.get_axis(1)) > c.THRESHOLD:
+							cim_lef.set(stick.get_axis(1) * -1)
+							cim_rig.set(stick.get_axis(1))
+						else:
+							cim_lef.set(0)
+							cim_rig.set(0)
+					if i == 3:
+						if abs(stick.get_axis(3)) > c.THRESHOLD:
+							cim_top.set(stick.get_axis(3) * -1)
+							cim_bot.set(stick.get_axis(3))
+						else:
+							cim_top.set(0)
+							cim_bot.set(0)
 	except KeyboardInterrupt:
 		print "Exited by user request"
-		enabled = False
-		c.PWM.set_all_pwm(0, 0)
-		sys.exit()
-	except Exception as e:
-		print "".format(e.errno, e.strerror)
 		enabled = False
 		c.PWM.set_all_pwm(0, 0)
 		sys.exit()
