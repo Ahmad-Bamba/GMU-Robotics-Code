86901a930ff3ebf08d86c271dd887dd0aaa56c49
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-03-23 23:59:58 -0400

    Write to straight gpio pins

---
+++ presentation.py
@@ -0,0 +1,66 @@
+import RPi.GPIO as GPIO
+import pygame
+import sys
+
+frequency = 50
+motor_one_pin = 14
+motor_two_pin = 17
+right_stick_y = 5
+safe_sleep = 0.05
+stick_port = 0
+# pwm_min = 250
+# pwm_max = 410
+stick = None
+motor_one = None
+motor_two = None
+
+def init():
+    global motor_one
+    global motor_two
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(motor_one_pin, GPIO.OUT)
+    GPIO.setup(motor_two_pin, GPIO.OUT)
+    motor_one = GPIO.PWM(motor_one_pin, frequency)
+    motor_one.start(50)
+    motor_two = GPIO.PWM(motor_two_pin, frequency)
+    motor_two.start(50)
+
+    pygame.joystick.init()
+    pygame.display.init()
+    if not (pygame.joystick.get_count() > 0):
+        print "No joysticks detected, quiting..."
+        sys.exit()
+    stick = pygame.joystick.Joystick(c.JOYSTICK)
+    stick.init()
+
+def periodic():
+    enabled = True
+    while enabled:
+        try:
+            pygame.event.pump()
+            for event in pygame.event.get():
+                if pygame.event.event_name(event.type) == "JoyButtonUp":
+                    # Do button stuff here
+                    if int(event.button) == 0:  # A button
+                        print "A"
+                    elif int(event.button) == 1:  # B Button
+                        print "B"
+                    elif int(event.button) == 2:  # X Button?
+                        print "X"
+                    elif int(event.button) == 3:  # Y Button?
+                        print "Y"
+
+            motor_one.ChangeDutyCycle(50.0 + (50.0 * stick.get_axis(right_stick_y)))
+            motor_two.ChangeDutyCycle(50.0 + (50.0 * stick.get_axis(right_stick_y)))
+        except KeyboardInterrupt:
+            print "Keyboard Interrupt!"
+            motor_one.stop()
+            motor_two.stop()
+            GPIO.cleanup()
+            print "Closing presentation mode"
+            enabled = False
+
+if __name__ == '__main__':
+    init()
+    periodic()
--- robot.py
+++ robot.py
@@ -20,60 +20,61 @@ cim_lef = motor.Victor(c.MOTOR_LEFT)
 cim_rig = motor.Victor(c.MOTOR_RIGHT)
 
 def init():
-	global stick
+    global stick
 
-	pygame.joystick.init()
-	pygame.display.init()
-	if not (pygame.joystick.get_count() > 0):
-		print "No joysticks detected, quiting..."
-		sys.exit()
-	stick = pygame.joystick.Joystick(c.JOYSTICK)
-	stick.init()
+    pygame.joystick.init()
+    pygame.display.init()
+    if not (pygame.joystick.get_count() > 0):
+        print "No joysticks detected, quiting..."
+        sys.exit()
+    stick = pygame.joystick.Joystick(c.JOYSTICK)
+    stick.init()
 
 def periodic():
-	global stick
+    global stick
+    global enabled
 
-	enabled = True
-	try:
-		while enabled:
-			pygame.event.pump()
-			for event in pygame.event.get():
-				if pygame.event.event_name(event.type) == "JoyButtonUp":
-					# Do button stuff here
-					if int(event.button) == 0: # A button
-						print "A"
-					elif int(event.button) == 1: # B Button
-						print "B"
-					elif int(event.button) == 2: # X Button?
-						print "X"
-					elif int(event.button) == 3: # Y Button?
-						print "Y"
+    enabled = True
+    try:
+        while enabled:
+            pygame.event.pump()
+            for event in pygame.event.get():
+                if pygame.event.event_name(event.type) == "JoyButtonUp":
+                    # Do button stuff here
+                    if int(event.button) == 0: # A button
+                        print "A"
+                    elif int(event.button) == 1: # B Button
+                        print "B"
+                    elif int(event.button) == 2: # X Button?
+                        print "X"
+                    elif int(event.button) == 3: # Y Button?
+                        print "Y"
 
-			for i in xrange(stick.get_numaxes()):
-				if i == 1:
-					if abs(stick.get_axis(1)) > c.THRESHOLD:
-						cim_lef.set(stick.get_axis(1) * -1)
-						cim_rig.set(stick.get_axis(1))
-					else:
-						cim_lef.set(0)
-						cim_rig.set(0)
-				if i == 3:
-					if abs(stick.get_axis(3)) > c.THRESHOLD:
-						cim_top.set(stick.get_axis(3) * -1)
-						cim_bot.set(stick.get_axis(3))
-					else:
-						cim_top.set(0)
-						cim_bot.set(0)
-	except KeyboardInterrupt:
-		print "Exited by user request"
-		enabled = False
-		c.PWM.set_all_pwm(0, 0)
-		sys.exit()
-	except Exception as e:
-		print "".format(e.errno, e.strerror)
-		enabled = False
-		c.PWM.set_all_pwm(0, 0)
-		sys.exit()
+            for i in xrange(stick.get_numaxes()):
+                if i == 1:
+                    if abs(stick.get_axis(1)) > c.THRESHOLD:
+                        cim_lef.set(stick.get_axis(1) * -1)
+                        cim_rig.set(stick.get_axis(1))
+                    else:
+                        cim_lef.set(0)
+                        cim_rig.set(0)
+                if i == 3:
+                    if abs(stick.get_axis(3)) > c.THRESHOLD:
+                        cim_top.set(stick.get_axis(3) * -1)
+                        cim_bot.set(stick.get_axis(3))
+                    else:
+                        cim_top.set(0)
+                        cim_bot.set(0)
+    except KeyboardInterrupt:
+        print "Exited by user request"
+        enabled = False
+        c.PWM.set_all_pwm(0, 0)
+        sys.exit()
+    except Exception as e:
+        print "".format(e.errno, e.strerror)
+        enabled = False
+        c.PWM.set_all_pwm(0, 0)
+        sys.exit()
 
 init()
 periodic()
